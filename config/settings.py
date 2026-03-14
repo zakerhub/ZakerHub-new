0d9c58d0ff8c697013727b61675890f01b26d793
unknown <newl4533@gmail.com>
2026-03-14 16:03:12 +0200

    database

--- config/settings.py
+++ config/settings.py
@@ -96,7 +96,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600
     )
 }
