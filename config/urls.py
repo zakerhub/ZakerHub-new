bdb36ba930296e22b587a83f665880f912937c61
unknown <newl4533@gmail.com>
2026-03-24 13:14:59 +0200

    fixing media files

--- config/urls.py
+++ config/urls.py
@@ -66,5 +66,5 @@ urlpatterns = [
     path('api/v1/subscriptions/', include('subscriptions.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files (uploaded content) in all environments
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
